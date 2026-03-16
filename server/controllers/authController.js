1104055c2f2dbc0feb08b98ae3f01d1c542f3adc
Draxie232 <bhuwanca11@gmail.com>
2026-03-16 14:26:13 +0530

    authController updated

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,7 +1,43 @@
-const signup = (req, res) => {
-  res.json({
-    message: "Signup route working"
-  });
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
+const signup = async (req, res) => {
+
+  try {
+
+    const { email, password } = req.body;
+
+    // Check if email already exists
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists"
+      });
+    }
+
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create user
+    const user = new User({
+      email,
+      password: hashedPassword
+    });
+
+    await user.save();
+
+    res.json({
+      message: "User registered successfully"
+    });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Server error"
+    });
+  }
+
 };
 
 module.exports = { signup };
